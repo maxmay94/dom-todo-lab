3891ecebe0d9c1994eb79b873d26e5935506c6ce
maxmay <maxmay94@gmail.com>
2022-02-02 17:45:27 -0500

    solve parts 5, 6, and add delete on click

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 console.log('DOM Todo')
+
+const usrIn = document.getElementById('user-input')
+const btn = document.getElementById('submit-button')
+const myList = document.getElementById('todo-list')
+
+btn.addEventListener('click', addToList)
+
+function addToList(evt) {
+  let newLi = document.createElement('li')
+  if(usrIn.value !== '') {
+    newLi.textContent = usrIn.value
+    myList.appendChild(newLi)
+    usrIn.value = ''
+  }
+}
+
+myList.addEventListener('click', removeLi)
+
+function removeLi(evt) {
+  evt.target.parentNode.removeChild(evt.target)
+  console.log(evt.target)
+}
